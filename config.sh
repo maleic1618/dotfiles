21403b951b62ca95ed4b9d9a408bfec853bab142
maleic <maleic@fleur>
2018-07-03 04:10:19 +0900

    first commit

---
+++ config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ln -s ~/.dotfiles/init.el ~/.emacs.d/init.el
+ln -s ~/.dotfiles/.mew.el ~/.mew.el
+ln -s ~/.dotfiles/.bashrc ~/.bashrc
+ln -s ~/.dotfiles/redshift.conf ~/.config/redshift.conf
+ln -s ~/.dotfiles/.latexmkrc ~/.latexmkrc
+ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/.xmodmap ~/.xmodmap
+
